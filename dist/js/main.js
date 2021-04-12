682fc619fe22b02e46901294463c3acfbff49751
Rabbani <you@example.com>
2021-04-12 16:12:22 +0600

    make animate on scroll function

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,3 +18,24 @@ function navbarFixed() {
 navbarFixed();
 
 /*sticky navigation menu End*/
+
+/* Animate On Scroll Start */
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1300,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+/* Animate On Scroll End */
